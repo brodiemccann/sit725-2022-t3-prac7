07ae873d477dce09994b51a876cab5ef1951549c
brodiemccann <117808890+brodiemccann@users.noreply.github.com>
2022-11-20 17:25:33 +1100

    Updated Prac 2

--- server.js
+++ server.js
@@ -1,36 +1,22 @@
 var express = require("express")
 var app = express()
 
-app.use(express.static(__dirname+'/public'))
+app.use(express.static(__dirname + '/public'))
 app.use(express.json());
-app.use(express.urlencoded({extends: false}))
+app.use(express.urlencoded({ extends: false }))
 
 const addNumbers = (number1, number2) => {
     var num1 = parseInt(number1)
     var num2 = parseInt(number2)
-    var result = num1+num2;
-    return result; 
+    var result = num1 + num2;
+    return result;
 }
 
-app.get("/addTwoNumbers", (req,res) => {
+app.get("/addTwoNumbers", (req, res) => {
     var number1 = req.query.number1;
     var number2 = req.query.number2;
     var result = addNumbers(number1, number2)
-    res.json({statusCode: 200, data: result, message:'Success'})
-})
-
-app.post("/addTwoNumbers",(req,res) => {
-    var number1 = req.body.number1;
-    var number2 = req.body.number2;
-    var result = addNumbers(number1,number2)
-    res.json({statusCode: 200, data: result, message:"Success"})
-})
-
-app.post("/addTwoNumbers",(req,res) => {
-    var number1 = req.body.number1;
-    var number2 = req.body.number2;
-    var result = addNumbers(number1,number2)
-    res.json({statusCode: 200, data: result, message:"Success"})
+    res.json({ statusCode: 200, data: result, message: 'Success' })
 })
 
 var port = process.env.port || 3000;
@@ -38,3 +24,4 @@ var port = process.env.port || 3000;
 app.listen(port, () => {
     console.log("App listening to: http://localhost:" + port)
 })
+
