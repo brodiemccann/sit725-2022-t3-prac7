15c6284d28ffaa5dc8f847661dea4b560d4d1ea3
brodiemccann <117808890+brodiemccann@users.noreply.github.com>
2022-12-03 23:49:01 +1100

    P4 Updates

--- server.js
+++ server.js
@@ -1,27 +1,90 @@
 var express = require("express")
 var app = express()
+var cors = require("cors")
+let projectCollection;
 
 app.use(express.static(__dirname + '/public'))
 app.use(express.json());
-app.use(express.urlencoded({ extends: false }))
+app.use(express.urlencoded({ extended: false }));
+app.use(cors())
 
-// const addNumbers = (number1, number2) => {
-//     var num1 = parseInt(number1)
-//     var num2 = parseInt(number2)
-//     var result = num1 + num2;
-//     return result;
-// }
+//mongoDB connection
+const MongoClient = require('mongodb').MongoClient;
 
-// app.get("/addTwoNumbers", (req, res) => {
-//     var number1 = req.query.number1;
-//     var number2 = req.query.number2;
-//     var result = addNumbers(number1, number2)
-//     res.json({ statusCode: 200, data: result, message: 'Success' })
-// })
+// add database connections
+const uri = 'mongodb+srv://mccannbr:brodieSIT725@cluster0.xlkct3q.mongodb.net/?retryWrites=true&w=majority'
+const client = new MongoClient(uri, { useNewUrlParser: true })
+
+const insertProjects = (project, callback) => {
+    projectCollection.insert(project, callback);
+}
+
+const getProjects = (callback) => {
+    projectCollection.find({}).toArray(callback);
+}
+
+const createColllection = (collectionName) => {
+    client.connect((err, db) => {
+        projectCollection = client.db().collection(collectionName);
+        if (!err) {
+            console.log('MongoDB Connected')
+        }
+        else {
+            console.log("DB Error: ", err);
+            process.exit(1);
+        }
+    })
+}
+
+const cardList = [
+
+    {
+        title: "Tassie Devil",
+        image: "images/tassie-unsplash.jpg",
+        link: "About Tassie Devil",
+        desciption: "Demo desciption about Tassie Devil"
+
+    },
+
+    {
+        title: "Dingo",
+        image: "images/dingo-unsplash.jpg",
+        link: "About Dingo",
+        desciption: "Demo desciption about Dingo"
+    }
+
+]
+
+app.get('/api/projects', (req, res) => {
+    getProjects((err, result) => {
+        if (err) {
+            res.json({ statusCode: 400, message: err })
+        }
+        else {
+            res.json({ statusCode: 200, message: "Success", data: result })
+        }
+    })
+})
+
+//add post api
+app.post('/api/projects', (req, res) => {
+    console.log("New Project added", req.body)
+    var newProject = req.body;
+    insertProjects(newProject, (err, result) => {
+        if (err) {
+            res.json({ statusCode: 400, message: err })
+        }
+        else {
+            res.json({ statusCode: 200, message: "Project Successfully added", data: result })
+        }
+    })
+})
+
 
 var port = process.env.port || 3000;
 
 app.listen(port, () => {
     console.log("App listening to: http://localhost:" + port)
+    createColllection("Australian Wildlife")
 })
 
