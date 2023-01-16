1aa96b509265391df1cdfb8f60e6d002bed0f208
brodiemccann <117808890+brodiemccann@users.noreply.github.com>
2023-01-16 21:45:37 +1100

    Updated P7

--- server.js
+++ server.js
@@ -1,25 +1,39 @@
 var express = require("express")
 var app = express()
 var cors = require('cors')
-let dbConnect = require("./dbConnect"); 
-let projectRoutes = require("./routes/projectRoutes"); 
+let dbConnect = require("./dbConnect");
+let projectRoutes = require("./routes/projectRoutes");
 // let projectCollection; 
+let http = require('http').createServer(app);
+let io = require('socket.io')(http);
 
-app.use(express.static(__dirname+'/public'))
+app.use(express.static(__dirname + '/public'))
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors())
 app.use('/api/projects', projectRoutes)
 
-app.get('/addTwoNumbers/:firstNumber/:secondNumber', function(req,res,next){
-    var firstNumber = parseInt(req.params.firstNumber) 
-    var secondNumber = parseInt(req.params.secondNumber)
-    var result = firstNumber + secondNumber || null
-    if(result == null) {
-      res.json({result: result, statusCode: 400}).status(400)
-    }
-    else { res.json({result: result, statusCode: 200}).status(200) } 
-  })
+app.get('/addTwoNumbers/:firstNumber/:secondNumber', function (req, res, next) {
+  var firstNumber = parseInt(req.params.firstNumber)
+  var secondNumber = parseInt(req.params.secondNumber)
+  var result = firstNumber + secondNumber || null
+  if (result == null) {
+    res.json({ result: result, statusCode: 400 }).status(400)
+  }
+  else { res.json({ result: result, statusCode: 200 }).status(200) }
+})
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+
+  setInterval(() => {
+    socket.emit('number', parseInt(Math.random() * 10));
+  }, 1000);
+
+});
 
 // //mongoDb connection...
 // const MongoClient = require('mongodb').MongoClient; 
@@ -75,8 +89,8 @@ app.get('/addTwoNumbers/:firstNumber/:secondNumber', function(req,res,next){
 //     })
 // })
 
-var port = process.env.port || 8080;
-app.listen(port,()=>{
-    console.log("App listening to http://localhost:"+port)
-    //createCollection('Australian Wildlife')
+var port = process.env.port || 3000;
+http.listen(port, () => {
+  console.log("App listening to http://localhost:" + port)
+  //createCollection('Australian Wildlife')
 })
